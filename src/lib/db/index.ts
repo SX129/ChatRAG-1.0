be356d2263f4aece9c9fd0908b9ca183318a4fcd
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-17 15:17:35 -0600

    Initialized neonDB and drizzle functionality

---
+++ src/lib/db/index.ts
@@ -0,0 +1,18 @@
+import {neon, neonConfig} from '@neondatabase/serverless';
+import {drizzle} from 'drizzle-orm/neon-http';
+
+/*
+Setting up neon database configuration:
+Caches connections being set.
+*/
+neonConfig.fetchConnectionCache = true;
+
+if (!process.env.DATABASE_URL){
+    throw new Error('Database URL not found.');
+}
+
+/* Connecting neon database to url */
+const sql = neon(process.env.DATABASE_URL);
+
+/* Drizzle function allows variable to call SQL functions on the database */
+export const db = drizzle(sql);
