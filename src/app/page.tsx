ab32dfb51aa17363136e599f5a5f596a0248ece0
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-17 14:22:10 -0600

    Added headers and buttons for auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,41 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
 
-export default function Home() {
+//Server component used to render initial client homepage
+export default async function Home() {
+  //Obtaining userID from clerk authentication and transforming it to a boolean
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
   return (
-    <Button> Click Me </Button>
-  )
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold"> ChatRAG </h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth && <Button> Go to Chats </Button>}
+          </div>
+
+          <p className="max-w-xl text-lg mt-1 text-slate-600">
+            Let AI read your PDFs for you!
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>file-upload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>Login to get Started</Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
