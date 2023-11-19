cca22424819bcee7c9273c6f58514bad92802c4d
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-18 18:28:53 -0600

    Created user file upload component for pdf

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {useDropzone} from 'react-dropzone';
+
+/* User file upload component */
+const FileUpload = () => {
+  return (
+    <div className='p-2 bg-white rounded-xl'></div>
+  )
+}
+
+export default FileUpload
