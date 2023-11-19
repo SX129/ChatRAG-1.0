8d27a5d61514d8cf94d6fe1c9c6047fc7ba68180
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-18 19:05:09 -0600

    Added configs for dropzone

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,10 +1,17 @@
-'use client';
+"use client";
+import { Inbox } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
 
 /* User file upload component */
 const FileUpload = () => {
-  const { getRootProps, getInputProps } = useDropzone();
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {"application/pdf": [".pdf"]},
+    maxFiles: 1,
+    onDrop: (acceptedFiles) => {
+        console.log(acceptedFiles);
+    },
+  });
   return (
     <div className="p-2 bg-white rounded-xl">
       <div
@@ -13,7 +20,11 @@ const FileUpload = () => {
             "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
         })}
       >
-        <input {...getInputProps} />
+        <input {...getInputProps()} />
+        <>
+          <Inbox className="w-10 h-10 text-blue-500" />
+          <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+        </>
       </div>
     </div>
   );
