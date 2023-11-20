763958fddefaeb86b401a8b9579267e447e6ac7b
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-20 12:34:50 -0600

    Created a providers component to wrap the app

---
+++ src/components/Providers.tsx
@@ -0,0 +1,13 @@
+'use client'
+// All client components must have the above string to define itself
+import React from 'react'
+
+type Props = {}
+
+const Providers = (props: Props) => {
+  return (
+    <div>Providers</div>
+  )
+}
+
+export default Providers
