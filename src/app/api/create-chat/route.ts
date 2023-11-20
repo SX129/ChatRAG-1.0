b6e86f273910cef6e76fce99a9a533a661ad1c57
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-20 12:32:44 -0600

    Refactored code

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -5,8 +5,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request, res: Response) {
   try {
     const body = await req.json();
-    const {file_key, file_name} = body;
-
+    const { file_key, file_name } = body;
   } catch (error) {
     console.error(error);
     return NextResponse.json(
