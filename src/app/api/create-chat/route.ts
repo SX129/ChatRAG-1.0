19d164c002e62d7b0afdc51dbcfbdf2e2c4fe77c
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-20 17:59:17 -0600

    Imported pinecone function

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,3 +1,4 @@
+import { loadS3IntoPinecone } from "@/lib/pinecone";
 import { NextResponse } from "next/server";
 
 /* File structure of route.ts maps to endpoint /api/create-chat */
@@ -7,6 +8,7 @@ export async function POST(req: Request, res: Response) {
     const body = await req.json();
     const { file_key, file_name } = body;
     console.log(file_key, file_name);
+    await loadS3IntoPinecone(file_key);
     return NextResponse.json({message: 'Success'});
   } catch (error) {
     console.error(error);
