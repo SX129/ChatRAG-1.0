ee778ae73efd851c320e78d998e40b3d3aa921a3
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-20 18:19:30 -0600

    Downloading and reading pdf from s3

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,4 +1,6 @@
 import { Pinecone } from '@pinecone-database/pinecone';
+import { downloadFromS3 } from './s3-server';
+import {PDFLoader} from 'langchain/document_loaders/fs/pdf';
 
 //Initializing pinecone client connection
 export const pinecone = new Pinecone({ 
@@ -8,4 +10,16 @@ export const pinecone = new Pinecone({
 
 export async function loadS3IntoPinecone(fileKey: string){
 
+    //Downloading PDF from S3
+    console.log('Downloading S3 pdf into file system.');
+    const file_name = await downloadFromS3(fileKey);
+
+    if (!file_name){
+        throw new Error('Could not download from S3.');
+    }
+
+    //Reading PDF
+    const loader = new PDFLoader(file_name);
+    const pages = loader.load();
+    return pages;
 };
