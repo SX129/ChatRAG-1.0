f4712b75188d1ca886e38b85185762049deb8807
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-21 22:56:56 -0600

    Added util function to remove non-ascii characters

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 };
 
-//Converting file key into ASCII
+//Removes non-ASCII characters
 export function convertToAscii(inputString: string){
   const asciiString = inputString.replace(/[^\x00-\x7F]+/g, '');
 };
