e24c15b79c0ee1dc168ecebc09ac44cefc858962
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-22 15:12:06 -0600

    Created chat message endpoint for openai api

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,24 @@
+import {Configuration, OpenAIApi} from 'openai-edge';
+
+export const runtime = 'edge'
+
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+//Endpoint for chat messages
+export async function POST(req: Request){
+    try {
+        const {messages} = await req.json();
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages,
+            stream: true,
+        });
+    } catch (error) {
+        
+    }
+}
