51c7898d5d5ccbe940ba1007a2d70d36f881bb48
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-21 21:00:12 -0600

    Added console log

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -49,7 +49,8 @@ const FileUpload = () => {
         }
         mutate(data, {
           onSuccess: (data) => {
-            toast.success(data.message);
+            //toast.success(data.message);
+            console.log(data);
           },
           onError: (error) => {
             toast.error("Error creating chat.");
