7c61ce5386ef803da5c25a0f2efa07d2261ab4f4
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-22 15:20:41 -0600

    Created function to obtain metadata from query

---
+++ src/lib/context.ts
@@ -0,0 +1,9 @@
+//Obtaining the most similar vector embeddings from user query
+export async function getMatchesFromEmbeddings(embeddings: number[], fileKey: string){
+
+}
+
+//Extracting the metadata from vector embeddings
+export async function getContext(query: string, fileKey: string){
+
+}
