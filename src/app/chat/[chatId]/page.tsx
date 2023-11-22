21b45488bb98039fe408a4564a142dc97efb07c3
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-22 00:03:25 -0600

    Chat validation with schema

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,4 +1,7 @@
+import { db } from '@/lib/db';
+import { chats } from '@/lib/db/schema';
 import { auth } from '@clerk/nextjs';
+import { eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
 import React from 'react'
 
@@ -14,9 +17,28 @@ const ChatPage = async ({params: {chatId}}: Props) => {
         return redirect('/sign-in');
     }
 
-    
+    //SQL statement (chats is schema)
+    const _chats = await db.select().from(chats).where(eq(chats.userId, userId));
+
+    //_chats is a list of chats
+    if(!_chats){
+        return redirect('/');
+    }
+
+    if(!_chats.find(chat => chat.id === parseInt(chatId))){
+        return redirect('/');
+    }
   return (
-    <div>{chatId}</div>
+    <div className='flex max-h-screen overflow-scroll'>
+        <div className='flex w-full max-h-screen overflow-scroll'>
+            {/* chat history sidebar */}
+            <div></div>
+            {/* middle pdf viewer */}
+            <div></div>
+            {/* chat message component */}
+            <div></div>
+        </div>
+    </div>
   )
 }
 
