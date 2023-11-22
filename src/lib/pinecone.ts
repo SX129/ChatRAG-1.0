c57f4a6f1fe88b1c7b587d4efbcb6fbc043e9308
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-21 22:14:57 -0600

    Added functions to split and encode pdf text

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,7 @@
 import { Pinecone } from '@pinecone-database/pinecone';
 import { downloadFromS3 } from './s3-server';
 import {PDFLoader} from 'langchain/document_loaders/fs/pdf';
+import {Document, RecursiveCharacterTextSplitter} from '@pinecone-database/doc-splitter';
 
 //Initializing pinecone client connection
 export const pinecone = new Pinecone({ 
@@ -29,5 +30,34 @@ export async function loadS3IntoPinecone(fileKey: string){
     //Reading PDF
     const loader = new PDFLoader(file_name);
     const pages = (await loader.load()) as PDFPage[];
+
+    //Splitting PDF pages into smaller documents (vectors)
+
+
+
     return pages;
 };
+
+//Ensuring text does not exceed maximum bytes
+export const truncateStringByBytes = (str: string, bytes: number) => {
+    const enc = new TextEncoder();
+    return new TextDecoder('utf-8').decode(enc.encode(str).slice(0, bytes));
+}
+
+//Splitting PDF
+async function prepareDocument(page: PDFPage){
+    let {pageContent, metadata} = page;
+
+    pageContent = pageContent.replace(/\n/g, '');
+    const splitter = new RecursiveCharacterTextSplitter();
+    const docs = await splitter.splitDocuments([
+        new Document({
+            pageContent,
+            metadata: {
+                pageNumber: metadata.loc.pageNumber,
+                text: truncateStringByBytes(pageContent, 36000),
+            }
+        })
+    ])
+    return docs;
+}
