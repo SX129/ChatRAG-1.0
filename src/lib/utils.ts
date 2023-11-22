a41945bdf9a55c3843f2b2cb4eb40ee382e2e09f
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-22 17:42:09 -0600

    returned function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,4 +8,5 @@ export function cn(...inputs: ClassValue[]) {
 //Removes non-ASCII characters
 export function convertToAscii(inputString: string){
   const asciiString = inputString.replace(/[^\x00-\x7F]+/g, '');
+  return asciiString;
 };
