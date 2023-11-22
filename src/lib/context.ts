86e33d4a2f3df1a4ee2e4cf53f03414583d7f8f7
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-22 15:49:31 -0600

    Added maximum character limit

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -43,5 +43,5 @@ export async function getContext(query: string, fileKey: string){
 
     let docs = qualifyingDocs.map(match => (match.metadata as Metadata).text);
 
-    return docs.join('\n');
+    return docs.join('\n').substring(0,3000);
 }
