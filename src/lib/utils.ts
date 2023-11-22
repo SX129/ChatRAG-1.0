ad6e3e0d50ebd16c97086fbd8ab186685b40db0d
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-21 22:56:00 -0600

    Added util function to convert string to ascii

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,4 +3,9 @@ import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
+};
+
+//Converting file key into ASCII
+export function convertToAscii(inputString: string){
+  const asciiString = inputString.replace(/[^\x00-\x7F]+/g, '');
+};
