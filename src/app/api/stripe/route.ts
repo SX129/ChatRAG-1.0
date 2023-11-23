947e8fb4e72d953f1295204827bfdb158a92f673
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-22 20:03:21 -0600

    Created stripe endpoint with subscription logic

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,64 @@
+import { db } from "@/lib/db";
+import { userSubscriptions } from "@/lib/db/schema";
+import { stripe } from "@/lib/stripe";
+import { auth, currentUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+
+const return_url = process.env.NEXT_BASE_URL + '/';
+
+export async function GET(){
+    try {
+        const {userId} = await auth();
+        const user = await currentUser();
+
+        if(!userId){
+            return new NextResponse('Unauthorized', {status: 401})
+        };
+
+        const _userSubscriptions = await db.select().from(userSubscriptions).where(eq(userSubscriptions.userId, userId));
+
+        //User cancelling subscription
+        if(_userSubscriptions[0] && _userSubscriptions[0].stripeCustomerId){
+            const stripeSession = await stripe.billingPortal.sessions.create({
+                customer: _userSubscriptions[0].stripeCustomerId,
+                return_url,
+            });
+            return NextResponse.json({url: stripeSession.url});
+        };
+
+        //User subscribing
+        const stripeSession = await stripe.checkout.sessions.create({
+            success_url: return_url,
+            cancel_url: return_url,
+            payment_method_types: ['card'],
+            mode: 'subscription',
+            billing_address_collection: 'auto',
+            customer_email: user?.emailAddresses[0].emailAddress,
+            line_items: [
+                {
+                    price_data: {
+                        currency: 'USD',
+                        product_data: {
+                            name: "ChatRAG Pro",
+                            description: "Unlimited PDF Sessions",
+                        },
+                        unit_amount: 2000,
+                        recurring: {
+                            interval: 'month'
+                        },
+                    },
+                    quantity: 1,
+                }
+            ],
+            metadata: {
+                userId,
+            },
+        });
+
+        return NextResponse.json({url: stripeSession.url});
+    } catch (error) {
+        console.log("Stripe error.", error);
+        return new NextResponse("Internal Server Error", {status: 500});
+    }
+}
