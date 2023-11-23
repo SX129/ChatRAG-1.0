666107b1a971873280f3276ace71f44ba9d1f883
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-22 19:46:17 -0600

    Added schema for stripe subscriptions

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -25,3 +25,13 @@ export const messages = pgTable('messages', {
     createdAt: timestamp('created_at').notNull().defaultNow(),
     role: userSystemEnum('role').notNull(),
 });
+
+/* Schema for stripe user subscription */
+export const userSubscriptions = pgTable('user_subscriptions', {
+    id: serial('id').primaryKey(),
+    userId: varchar('user_id', {length: 256}).notNull().unique(),
+    stripeCustomerId: varchar('stripe_customer_id', {length: 256}).notNull().unique(),
+    stripeSubscriptionId: varchar('stripe_subscription_id', {length: 256}).unique(),
+    stripePriceId: varchar('stripe_price_id', {length: 256}),
+    stripeCurrentPeriodEnd: timestamp('stripe_current_period_end'),
+});
