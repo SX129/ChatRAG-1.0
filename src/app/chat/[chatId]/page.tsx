78687e6cdefb38d8921ac84d9546e82fdf9d83a0
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-22 21:04:27 -0600

    Added subscription validation

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -3,6 +3,7 @@ import ChatSideBar from '@/components/ChatSideBar';
 import PDFViewer from '@/components/PDFViewer';
 import { db } from '@/lib/db';
 import { chats } from '@/lib/db/schema';
+import { checkSubscription } from '@/lib/subscription';
 import { auth } from '@clerk/nextjs';
 import { eq } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
@@ -35,11 +36,13 @@ const ChatPage = async ({params: {chatId}}: Props) => {
     //Used to obtain pdf url
     const currentChat = _chats.find(chat => chat.id === parseInt(chatId));
 
+    const isPro = await checkSubscription();
+
   return (
     <div className='flex max-h-screen overflow-scroll'>
         <div className='flex w-full max-h-screen overflow-scroll'>
             <div className='flex-[1] max-w-xs'>
-                <ChatSideBar chats={_chats} chatId={parseInt(chatId)}/>
+                <ChatSideBar chats={_chats} chatId={parseInt(chatId)} isPro={isPro}/>
             </div>
             <div className='max-h-screen p-4 overflow-scroll flex-[5]'>
                 <PDFViewer pdf_url={currentChat?.pdfUrl || ''} />
